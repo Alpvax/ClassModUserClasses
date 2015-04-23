057ab807c71532b56309ff592ed7114d47ce4093
Alpvax <Alpvax@netscape.net>
2015-04-23 08:19:38 +0100

    Added Demon

---
+++ src/main/java/alpvax/classmoduc/classes/Demon.java
@@ -0,0 +1,35 @@
+package alpvax.classmoduc.classes;
+
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import alpvax.classmodcore.api.classes.SimplePlayerClass;
+import alpvax.classmodcore.api.powers.PowerEntry;
+
+public class Demon extends SimplePlayerClass
+{
+
+	public Demon(String name)
+	{
+		super(name);// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<PowerEntry> getPowers()
+	{
+		return null;// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void setup(EntityPlayer player)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void reset(EntityPlayer player)
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
