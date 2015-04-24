f865bfffc2076324cb07a0b4e6bac032b03785a5
Alpvax <Alpvax@netscape.net>
2015-04-24 08:43:08 +0100

    Fixed class registering

--- src/main/java/alpvax/classmoduc/core/CMUserClasses.java
+++ src/main/java/alpvax/classmoduc/core/CMUserClasses.java
@@ -33,7 +33,7 @@ public class CMUserClasses
 	@EventHandler
 	public void Init(FMLInitializationEvent event)
 	{
-		PlayerClassRegistry.registerPlayerClass(new Demon("demon").setDisplayName("Demon"), "user", new UserClassPermission("Alpvax"));
+		PlayerClassRegistry.registerPlayerClass(new Demon("user.demon").setDisplayName("Demon"), new UserClassPermission("Alpvax"));
 		//TODO:PlayerClassRegistry.registerPlayerClass(new Dwarf(), "user", new UserClassPermission("Alpvax"));
 		//TODO:PlayerClassRegistry.registerPlayerClass(new EndSpawn(), "user", new UserClassPermission("Alpvax"));
 		//TODO:PlayerClassRegistry.registerPlayerClass(new WereWolf(), "user", new UserClassPermission("Alpvax"));
