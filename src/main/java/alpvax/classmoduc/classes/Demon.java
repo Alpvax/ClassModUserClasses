51ca7cdb00c5604ac025e2d58709646aa96770b8
Alpvax <alpvax@netscape.net>
2015-04-28 16:06:30 +0100

    Added water damage power to demon

--- src/main/java/alpvax/classmoduc/classes/Demon.java
+++ src/main/java/alpvax/classmoduc/classes/Demon.java
@@ -1,10 +1,12 @@
 package alpvax.classmoduc.classes;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.util.DamageSource;
 import alpvax.classmodcore.api.classes.SimplePlayerClass;
 import alpvax.classmodcore.api.powers.PowerEntry;
 import alpvax.classmodcore.api.powers.PowerFireResist;
 import alpvax.classmodcore.api.powers.PowerInMaterialMoveSpeed;
+import alpvax.classmodcore.api.powers.PowerInMaterialSelfDamage;
 
 
 public class Demon extends SimplePlayerClass
@@ -14,6 +16,7 @@ public class Demon extends SimplePlayerClass
 	{
 		super(name);
 		addPower(new PowerEntry.Passive(new PowerFireResist(0F)));
+		addPower(new PowerEntry.Passive(new PowerInMaterialSelfDamage("Water", DamageSource.drown, 2F, 20, Material.water)));//1 heart every second
 		addPower(new PowerEntry.Toggle(new PowerInMaterialMoveSpeed("Lava", 2F, Material.lava), 0));
 	}
 }
