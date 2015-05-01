2aa803cd419feea800f49a59b1f5f624582f6c5c
Alpvax <Alpvax@netscape.net>
2015-05-01 08:51:06 +0100

    Fixed powers

--- src/main/java/alpvax/classmoduc/classes/Demon.java
+++ src/main/java/alpvax/classmoduc/classes/Demon.java
@@ -15,8 +15,8 @@ public class Demon extends SimplePlayerClass
 	public Demon(String name)
 	{
 		super(name);
-		addPower(new PowerEntry.Passive(new PowerFireResist(0F)));
-		addPower(new PowerEntry.Passive(new PowerInMaterialSelfDamage("Water", DamageSource.drown, 2F, 20, Material.water)));//1 heart every second
-		addPower(new PowerEntry.Toggle(new PowerInMaterialMoveSpeed("Lava", 2F, Material.lava), 0));
+		addPower(new PowerEntry(new PowerFireResist(0F)).setStartActive());
+		addPower(new PowerEntry(new PowerInMaterialSelfDamage("Water", DamageSource.drown, 2F, 20, Material.water)));//1 heart every second (20 ticks)
+		addPower(new PowerEntry(new PowerInMaterialMoveSpeed("Lava", 2F, Material.lava)));
 	}
 }
